ef0cf892c4d038e8759d1c982e5a29e254506824
kukcat <galaxycraft3103@gmail.com>
2022-01-19 19:45:45 +0200

    finish lesson

---
+++ script.js
@@ -0,0 +1,74 @@
+// let z = 2;
+
+// for(let i = 0; i < 10; i++){
+//     console.log( z *= 2 )
+// }
+
+//=================================
+
+// let start = prompt()
+// let finish = prompt()
+
+// for(;start<=finish; start++){
+//      console.log(start)
+// }
+
+//=================================
+
+//let i = prompt('Введите число');
+
+// while(i == null){
+
+//let  i = prompt('Введите число');
+
+// }
+
+// console.log(i);
+//=================================
+
+// let i;
+
+// do{
+
+// i = prompt('Введите число');
+
+// }while(i == null)
+
+// console.log(i);
+//=================================
+
+// let w = prompt('Введите число')
+
+
+// while(isNaN(w)){
+//      console.log(isNaN(w))
+//      w = prompt('Введите число')  
+// }
+
+
+// console.log(isNaN(w))
+// console.log(w)
+
+//=================================
+
+// let N = prompt('Введите число N')
+// let A = prompt('Введите число A')
+
+// let result = 0;
+
+// for(let i = 0; i <= N; i++){
+//      result += A**i;
+// }
+
+// console.log(result);
+
+//=================================
+
+let N = prompt('Введите число N')
+let result = 1;
+
+for(let i = 1; i<=N; i++){
+     result *= i;
+}
+
+console.log(result)
