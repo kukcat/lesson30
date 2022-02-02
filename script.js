82e3782f5481376fc60cd730636cad8f2c897a54
kukcat <galaxycraft3103@gmail.com>
2022-02-02 16:09:49 +0200

    lesson 34

--- script.js
+++ script.js
@@ -174,24 +174,199 @@
 
 // =============================== Второе задание
 
-let ans = prompt('Введите число')
-let array = [];
+// let ans = prompt('Введите число')
+// let array = [];
 
-while(!isNaN(ans) && ans != '' && ans != null){
+// while(!isNaN(ans) && ans != '' && ans != null){
 
-     array.push(+ans);
-     ans = prompt('Введите число')
-}
+//      array.push(+ans);
+//      ans = prompt('Введите число')
+// }
 
-let result = 0;
+// let result = 0;
 
-console.log(array)
+// console.log(array)
 
-for (let i = 0; i < array.length; i++) {
+// for (let i = 0; i < array.length; i++) {
     
-     result += array[i];
-     console.log(array[i]);
+//      result += array[i];
+//      console.log(array[i]);
+
+// }
+
+
+// console.log(result)
+
+// let N = prompt()
+// let r = 0; 
+
+// for(let i = 1; i <= N; i++){
+
+//     // r += i**i
+    
+//     if( i%3 == 0 ){
+//         continue
+//     }
+
+//     console.log(i)
+
+// }
+
+//console.log(r)
+
+// let i = prompt()
+// console.log(i)
+
+// while(i == null){
+
+
+//     i = prompt()
+//     console.log(i)
+
+// }
+
+// let price = 10;
+
+// let n = 5; 
+// let r = 0;
+
+// for(let i = 0; i<=n; i++ ){
+
+//     r+= (n+i)**2
+
+//     console.log(r)
+// }
+
+// console.log(r)
+
+
+//let arr = [1,2,3,4,5,6,7,8,9];
+
+// let messages = [];
+// let message = prompt();
+
+
+// while(message != null){
+
+//     messages.push(message);
+//     message = prompt();
+
+// }
+
+// // messages.push(message);
+
+// console.log(messages)
+
+
+
+// let arr = ['Привет', 'Андрей'];
+
+// arr[0] = 2
+
+// console.log(arr);
+
+
+
+// function addition(){
+
+    
+
+//     let a = prompt('введите первое число')
+//     let b = prompt('введите второе число')
+//     let z = 0;
+
+//     for (let index = 0; index < 4; index++) {
+        
+//         z += index; 
+        
+//     }
+    
+//     console.log(z);
+    
+//     console.log(+a + +b)
+    
+//     console.log(i)
+// }
+
+// addition('строка', 1)
+
+// for(let i = 0; i < 5; i++){
+//     addition(5);
+// }
+
+// function onLoad(){
+//     console.log('При загрузке')
+// }
+
+// onLoad();
+
+
+function for13(){
+
+    let N = prompt('Введите число');
+    let sum = 0;
+    let k = 1;
+
+
+    for(let i = 1; i <= N; i++){
+
+        sum += (1+i/10)*k
+        k = -k;
+
+    }
+
 
 }
 
-console.log(result)
+function for38(){
+
+    let N = prompt('Введите число')
+    let result = 0
+    for(let i = 1; i <= N; i++){
+        
+        result += i**(N - (i - 1))
+
+    }
+
+}
+
+function for40(){
+
+    let A = prompt();
+    let B = prompt();
+    let count = 1;
+    for(; A <= B; A++){
+
+        for(let i = 0; i<count; i++ ){
+
+            console.log(A)
+
+        }
+
+        count++
+    }
+
+
+}
+
+let sumInput = () => {
+    let arr = prompt('Введите значение')
+    let sumArr = []
+
+    while (!isNaN(arr) && arr != "" && arr != null) {
+        
+        sumArr.push(Number(arr))
+        arr = prompt('Введите значение')
+        
+    }
+
+    console.log(sumArr)
+    let r = 0
+
+    for(let i = 0; i < sumArr.length; i++){
+        r += sumArr[i]
+    }
+
+    console.log(r)
+
+}
